d8b0489891c3e6f56e8c52e36a5b69f9c00f9d66
Tho Ha <tho.ha@gatech.edu>
2023-03-13 00:55:53 -0500

    Adding keymap for telescope keymap

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -22,6 +22,7 @@ vim.keymap.set('n', '<leader>/', function()
     })
 end, { desc = '[/] Fuzzily search in current buffer' })
 
+vim.keymap.set('n', '<leader>km', builtin.keymaps, { desc = 'View Keymaps' })
 vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
 vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
 vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
