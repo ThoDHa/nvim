5b12e52e5b8ccd6a75f822711a4b0cfda58da717
Tho Ha <tho.ha@gatech.edu>
2023-03-23 19:43:00 -0500

    Removed After, and moved plugins directory under Lua

--- lua/ThoDHa/init.lua
+++ lua/ThoDHa/init.lua
@@ -3,5 +3,5 @@ require("ThoDHa.set")
 vim.api.nvim_create_autocmd("BufReadPost",
     {
         pattern = {"*.json"},
-        command = "silent %!jq ."
+        command = "silent %!python3 -m json.tool"
     })
