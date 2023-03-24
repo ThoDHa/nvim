645a1268344c27cc91d8c8d7efd51f9a8559970e
Tho Ha <tho.ha@gatech.edu>
2023-03-23 20:36:43 -0500

    Moving after back...

--- lua/ThoDHa/init.lua
+++ lua/ThoDHa/init.lua
@@ -5,14 +5,21 @@ vim.api.nvim_create_autocmd("BufReadPost",
         pattern = {"*.json"},
         command = "silent %!python3 -m json.tool"
     })
-
 vim.api.nvim_create_autocmd("BufReadPost",
+    {
+        pattern = {"*"},
+        command = "set colorcolumn="
+    })
+
+    vim.api.nvim_create_autocmd("BufReadPost",
     {
         pattern = {"*.py"},
         command = "set colorcolumn=100"
     })
-vim.api.nvim_create_autocmd("BufReadPost",
+
+    vim.api.nvim_create_autocmd("BufReadPost",
     {
         pattern = {"*.md"},
         command = "set colorcolumn=80"
     })
+
