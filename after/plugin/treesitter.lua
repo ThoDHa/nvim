020c975c79461beed18c74dbd8365f89af4b9026
Tho Ha <tho.ha@gatech.edu>
2023-04-08 01:03:04 -0500

    Updated treesitter help to be vimdoc

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,6 +1,7 @@
 require'nvim-treesitter.configs'.setup {
 	-- A list of parser names, or "all" (the five listed parsers should always be installed)
-    ensure_installed = { 'help', 
+    ensure_installed = { 
+	    'vimdoc', 
             --'javascript', 
             --'typescript', 
             "c", 
