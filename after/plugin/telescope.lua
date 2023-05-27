42cca1ae836c48ffa1dea509ef0b55c9461102a4
Tho Ha <tho.ha@gatech.edu>
2023-05-26 19:16:57 -0500

    Adding in descriptions to keymaps

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -4,10 +4,10 @@ local builtin = require('telescope.builtin')
 pcall(require('telescope').load_extension, 'fzf')
  
 -- vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
--- vim.keymap.set('n', '<C-p>', builtin.git_files, {})
--- vim.keymap.set('n', '<leader>ps', function()
---	builtin.grep_string({ search = vim.fn.input("Grep > ") });
---end)
+vim.keymap.set('n', '<leader>g', builtin.git_files, { desc = '[S]earch [G]it Files'})
+vim.keymap.set('n', '<leader>ps', function()
+	builtin.grep_string({ search = vim.fn.input("Grep > ") });
+end, { desc = 'Fuzzy search'})
 
 
 
@@ -22,7 +22,7 @@ vim.keymap.set('n', '<leader>/', function()
     })
 end, { desc = '[/] Fuzzily search in current buffer' })
 
-vim.keymap.set('n', '<leader>km', builtin.keymaps, { desc = 'View Keymaps' })
+vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
 vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
 vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
 vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
--- lua/ThoDHa/remap.lua
+++ lua/ThoDHa/remap.lua
@@ -1,42 +1,44 @@
 vim.g.mapleader = " "
-vim.keymap.set("n", "-", vim.cmd.Ex)
+vim.keymap.set("n", "-", vim.cmd.Ex, { desc = 'Open up Explorer'})
 
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+-- Remap <C-c> to Escape
+vim.keymap.set("i", "<C-c>", "<Esc>")
+
+vim.keymap.set("v", "J", ":m '>+0<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-3<CR>gv=gv")
 
 vim.keymap.set("n", "J", "mzJ`z")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
 -- greatest remap ever
 -- Paste but don't put the current text into register
-vim.keymap.set("x", "<leader>x", "\"_dP")
+vim.keymap.set("x", "<leader>x", "\"_dP", { desc = 'Paste but don\'t copy' })
 
 -- next greatest remap ever : asbjornHaland
 -- Yank into system clipboard
-vim.keymap.set("n", "<leader>y", "\"+y")
-vim.keymap.set("v", "<leader>y", "\"+y")
-vim.keymap.set("n", "<leader>yy", "\"+Y")
+vim.keymap.set("n", "<leader>y", "\"+y", { desc = 'Yank into system clipboard' })
+vim.keymap.set("v", "<leader>y", "\"+y", { desc = 'Yank into system clipboard' })
+vim.keymap.set("n", "<leader>Y", "\"+Y", { desc = 'Yank line into system clipboard' })
 
 -- paste from system clipboard
-vim.keymap.set("n", "<leader>p", "\"+p")
-vim.keymap.set("v", "<leader>p", "\"+p")
-vim.keymap.set("n", "<leader>pp", "\"+P")
+vim.keymap.set("n", "<leader>p", "\"+p", { desc = 'Paste from system clipboard'})
+vim.keymap.set("v", "<leader>p", "\"+p", { desc = 'Paste from system clipboard'})
+vim.keymap.set("n", "<leader>P", "\"+P", { desc = 'Paste line from system clipboard'})
 
 
-vim.keymap.set("n", "<leader>d", "\"_d")
-vim.keymap.set("v", "<leader>d", "\"_d")
+vim.keymap.set("n", "<leader>d", "\"_d", { desc = 'Delete and don\'t yank to register'})
+vim.keymap.set("v", "<leader>d", "\"_d", { desc = 'Delete and don\'t yank to register'})
 
--- This is going to get me cancelled
-vim.keymap.set("i", "<C-c>", "<Esc>")
 
 vim.keymap.set("n", "Q", "<nop>")
-vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
+vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>", { desc = "Create new tmux silent quietly"})
 vim.keymap.set("n", "<leader>f", function()
 	vim.lsp.buf.format()
-end)
+end, { desc = 'Format file'})
 
 vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
 vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
