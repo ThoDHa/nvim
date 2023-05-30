0e99a605bcb8bdbc82d85406c49ec0aba6fb9083
Tho Ha <tho.ha@gatech.edu>
2023-05-30 10:26:02 -0500

    updating telescope config

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -4,12 +4,6 @@ local builtin = require('telescope.builtin')
 pcall(require('telescope').load_extension, 'fzf')
  
 -- vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-vim.keymap.set('n', '<leader>g', builtin.git_files, { desc = '[S]earch [G]it Files'})
-vim.keymap.set('n', '<leader>ps', function()
-	builtin.grep_string({ search = vim.fn.input("Grep > ") });
-end, { desc = 'Fuzzy search'})
-
-
 
 -- See `:help telescope.builtin`
 vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
@@ -24,6 +18,7 @@ end, { desc = '[/] Fuzzily search in current buffer' })
 
 vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
 vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
+vim.keymap.set('n', '<leader>sfg', builtin.git_files, { desc = '[S]earch [F]iles [G]it'})
 vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
 vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
 vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
