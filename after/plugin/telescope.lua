a85f3b23de6ff262b6b1f5b18de0a79c0253adf8
Tho Ha <tho.ha@gatech.edu>
2023-07-20 10:28:18 -0500

    Added a bunch of telescope things for git - Added find references/implementations for telescope too

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -6,8 +6,8 @@ pcall(require('telescope').load_extension, 'fzf')
 -- vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 
 -- See `:help telescope.builtin`
-vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
-vim.keymap.set('n', '<leader><space>', require('telescope.builtin').buffers, { desc = '[ ] Find existing buffers' })
+vim.keymap.set('n', '<leader>?', builtin.oldfiles, { desc = '[?] Find recently opened files' })
+vim.keymap.set('n', '<leader><space>', builtin.buffers, { desc = '[ ] Find existing buffers' })
 vim.keymap.set('n', '<leader>/', function()
     --   -- You can pass additional configuration to telescope to change theme, layout, etc.
     require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
@@ -18,11 +18,22 @@ end, { desc = '[/] Fuzzily search in current buffer' })
 
 vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
 vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
-vim.keymap.set('n', '<leader>sfg', builtin.git_files, { desc = '[S]earch [F]iles [G]it'})
 vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
 vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
 vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
 vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
+vim.keymap.set("n", "<leader>gr", builtin.lsp_references, { desc = '[G]et [R]eferences'})
+vim.keymap.set("n", "<leader>gi", builtin.lsp_implementations, { desc = '[G]et [I]mplementations'})
+vim.keymap.set("n", "<leader>ml", builtin.jumplist, { desc = '[M]ark [L]ist'})
+
+
+
+
+vim.keymap.set('n', '<leader>gfs', builtin.git_files, { desc = '[G]it [F]iles [S]earch '})
+vim.keymap.set("n", "<leader>gb", builtin.jumplist, { desc = '[G]it [B]ranches'})
+vim.keymap.set("n", "<leader>gs", builtin.jumplist, { desc = '[G]it [S]tatus'})
+vim.keymap.set("n", "<leader>gbc", builtin.jumplist, { desc = '[G]it [B]uffer [C]ommits'})
+vim.keymap.set("n", "<leader>gc", builtin.jumplist, { desc = '[G]it [C]ommits'})
 
 vim.api.nvim_create_augroup('startup', { clear = true })
 vim.api.nvim_create_autocmd('VimEnter', {
