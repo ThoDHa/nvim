01ec94872b9c9db187523dd147886955782b6a75
Tho Ha <ThoDHa@users.noreply.github.com>
2025-10-19 22:27:23 -0400

    MarkView toggle -> Enable

--- lua/thodha/init.lua
+++ lua/thodha/init.lua
@@ -23,10 +23,12 @@ vim.api.nvim_create_autocmd("CursorHoldI", {
 	pattern = { "*" },
 	command = "let &undolevels = &undolevels",
 })
+
+-- Show MarkdownPreview by default when opening up MD files.
 vim.api.nvim_create_autocmd("BufWinEnter", {
 	callback = function()
 		if vim.bo.filetype == "markdown" then
-			pcall(vim.cmd, "Markview toggle")
+			pcall(vim.cmd, "Markview Enable")
 		end
 	end,
 })
