4848713cede9f443ff8cb5b6670ab8f18e2c54cb
Tho Ha <ThoDHa@users.noreply.github.com>
2026-04-08 11:30:52 +0700

    fix: update deprecated diagnostic.goto_* to diagnostic.jump
    Replace vim.diagnostic.goto_prev/goto_next with
    vim.diagnostic.jump({ count = -1/1 }) per Neovim 0.11 API.

--- lua/thodha/remap.lua
+++ lua/thodha/remap.lua
@@ -32,8 +32,8 @@ vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 vim.keymap.set("n", "<leader>Z", "<c-w>_<c-w>|", { desc = "Maximize current window" })
 vim.keymap.set("n", "<leader>z", "<c-w>=", { desc = "Change all windows to same size" })
 
-vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous [D]iagnostic message" })
-vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next [D]iagnostic message" })
+vim.keymap.set("n", "[d", function() vim.diagnostic.jump({ count = -1 }) end, { desc = "Go to previous [D]iagnostic message" })
+vim.keymap.set("n", "]d", function() vim.diagnostic.jump({ count = 1 }) end, { desc = "Go to next [D]iagnostic message" })
 vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Show diagnostic [E]rror messages" })
 vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
 
